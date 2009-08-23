a55fe5547116026df25548dd6f4a340251449e42
Alberto Simoes <ambs@cpan.org>
2009-08-23 21:48:53 +0100

    URI Escape node names

--- lib/Quiki/Formatter.pm
+++ lib/Quiki/Formatter.pm
@@ -1,7 +1,11 @@
 package Quiki::Formatter;
 
-use CGI ':standard';
 use feature ':5.10';
+
+use CGI ':standard';
+use URI::Escape;
+
+
 
 # Formatter (format)
 #------------------------------------------------------------
@@ -60,12 +64,16 @@ sub _inlines {
        ## [[http://foo]] -- same as http://foo ?
        qr/\[\[(\w+:\/\/[^\]|]+)\]\]/            => sub { a({-href=>$1}, $1) },
        ## [[nodo]]
-       qr/\[\[([^\]|]+)\]\]/                    => sub { a({-href=>"$script?node=$1"}, $1) },
+       qr/\[\[([^\]|]+)\]\]/                    => sub {
+           a({-href=>"$script?node=$1"}, uri_escape($1))
+       },
        ## [[protocol://foo|descricao]]
-       qr/\[\[(\w+:\/\/[^\]|]+)\|([^\]|]+)\]\]/ => sub { a({-href=>$1}, _inlines($Quiki, $2)) },
+       qr/\[\[(\w+:\/\/[^\]|]+)\|([^\]|]+)\]\]/ => sub {
+           a({-href=>$1}, _inlines($Quiki, $2))
+       },
        ## [[nodo|descricao]]
        qr/\[\[([^\]|]+)\|([^\]|]+)\]\]/         => sub {
-           a({-href=>"$script?node=$1"}, _inlines($Quiki, $2))
+           a({-href=>"$script?node=$1"}, _inlines($Quiki, uri_escape($2)))
        },
        ## ** foo **
        qr/\*\* ((?:\\\*|[^*]|\*[^*])+) \*\*/x   => sub { b(_inlines($Quiki, $1)) },
