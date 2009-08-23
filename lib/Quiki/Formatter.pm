31518e68fbd189a7c3d452217175b8a61fc280fa
Alberto Simoes <ambs@cpan.org>
2009-08-23 16:55:12 +0100

    Formatter.pm stub file.

---
+++ lib/Quiki/Formatter.pm
@@ -0,0 +1,46 @@
+package Quiki::Formatter;
+
+use CGI ':standard';
+
+sub format {
+    my $string = shift;
+    my $html = pre($string);
+    return $html;
+}
+
+
+"false";
+
+=head1 NAME
+
+Quiki::Formatter - Quiki formatter module
+
+=head1 SYNOPSIS
+
+  use Quiki::Formatter;
+  my $html = Quiki::Formatter::format($string);
+
+=head1 DESCRIPTION
+
+Hides formatting subroutine.
+
+=head1 SEE ALSO
+
+Quiki, perl(1)
+
+=head1 AUTHOR
+
+Alberto Simões, E<lt>ambs@cpan.orgE<gt>
+Nuno Carvalho, E<lt>smash@cpan.orgE<gt>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Alberto Simoes and Nuno Carvalho.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
