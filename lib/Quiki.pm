98efff88e552d3024af27e8c74977cd2d6685bbf
Alberto Simoes <ambs@cpan.org>
2009-08-24 12:22:35 +0100

    use CGI to create HTML.

--- lib/Quiki.pm
+++ lib/Quiki.pm
@@ -119,7 +119,7 @@ sub run {
         print start_form(-method=>'post'),
           textarea('text',$content,15,80),
             hidden('node',$node),
-              "<input type='hidden' name='action' value='save' />",
+              hidden('action','save'),
                 hr,
                   submit('submit', 'save'),
                     end_form;
