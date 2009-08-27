1b530b09b972bab3974d851221aec0afcc21eb14
Alberto Simoes <ambs@cpan.org>
2009-08-27 21:18:08 +0100

    Added function to test user existence

--- lib/Quiki/Users.pm
+++ lib/Quiki/Users.pm
@@ -6,10 +6,24 @@ use warnings;
 use DBI;
 use Digest::MD5 'md5_hex';
 
+sub _connect {
+    return DBI->connect("dbi:SQLite:dbname=data/users.sqlite","","");
+}
+
+sub exists {
+    my ($class, $username) = @_;
+    my $dbh = _connect;
+    my $sth = $dbh->prepare("SELECT username FROM auth WHERE username = ?");
+    $sth->execute($username);
+
+    my @row = $sth->fetchrow_array;
+    return (@row)?1:0;
+}
+
 sub auth {
     my ($class, $username, $password) = @_;
 
-    my $dbh = DBI->connect("dbi:SQLite:dbname=data/users.sqlite","","");
+    my $dbh = _connect;
     my $sth = $dbh->prepare("SELECT password FROM auth WHERE username = ?");
     $sth->execute($username);
 
@@ -34,13 +48,18 @@ Quiki::Users - Quiki users manager
   use Quiki::Users;
 
   # authenticate user
-  Quiki::Users -> auth($username, $passwod);
+  if (Quiki::Users -> auth($username, $passwod)) { ... }
+
+  # check user availability
+  if (not Quiki::Users -> exists($username)) { ... }
 
 =head1 DESCRIPTION
 
 Handles Quiki users and permissions.
 
 =head2 auth
+
+=head2 exists
 
 =head1 SEE ALSO
 
