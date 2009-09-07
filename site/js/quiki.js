6d8aceb5c4a77e4a1a23b522140a6d0088eac51a
Alberto Simoes <ambs@cpan.org>
2009-09-07 13:16:28 +0100

    new modal windows

---
+++ site/js/quiki.js
@@ -0,0 +1,12 @@
+
+function revealModal(divID)
+{
+    window.onscroll = function () { document.getElementById(divID).style.top = document.body.scrollTop; };
+    document.getElementById(divID).style.display = "block";
+    document.getElementById(divID).style.top = document.body.scrollTop;
+}
+
+function hideModal(divID)
+{
+    document.getElementById(divID).style.display = "none";
+}
