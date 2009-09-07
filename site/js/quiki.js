c00608f9f50d88bed284aab42f63f565257a2391
Alberto Simoes <ambs@cpan.org>
2009-09-07 21:19:11 +0100

    Added JS to automatically guess identifier

--- site/js/quiki.js
+++ site/js/quiki.js
@@ -1,7 +1,9 @@
 
 function revealModal(divID)
 {
-    window.onscroll = function () { document.getElementById(divID).style.top = document.body.scrollTop; };
+    window.onscroll = function () {
+        document.getElementById(divID).style.top = document.body.scrollTop;
+    };
     document.getElementById(divID).style.display = "block";
     document.getElementById(divID).style.top = document.body.scrollTop;
 }
@@ -10,3 +12,14 @@ function hideModal(divID)
 {
     document.getElementById(divID).style.display = "none";
 }
+
+function updateField(filefield, number) {
+    var name = document.getElementById( "name" + number );
+    if (! name.value && filefield.value) {
+        var temp = filefield.value;
+        temp = temp.replace(/.*[\\\/]/, "");
+        temp = temp.replace(/\..*$/, "");
+        name.value = temp;
+    }
+}
+
