641d1437145442903a5942a5486d3080cdb54d66
Alberto Simoes <ambs@cpan.org>
2009-09-09 12:18:00 +0100

    Javascript to insert at cursor

--- site/js/quiki.js
+++ site/js/quiki.js
@@ -1,4 +1,26 @@
 
+function insertAtCursor(Id, myValue) {
+    var myField = document.getElementById(Id);
+    //IE support
+    if (document.selection) {
+        myField.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+    }
+    //MOZILLA/NETSCAPE support
+    else if (myField.selectionStart || myField.selectionStart == '0') {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+        myField.value = myField.value.substring(0, startPos)
+            + myValue
+            + myField.value.substring(endPos, myField.value.length);
+    } else {
+        myField.value += myValue;
+    }
+}
+
+
+
 function revealModal(divID)
 {
     window.onscroll = function () {
